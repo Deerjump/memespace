8df5f3b20fb9ffd510853d71191e1bdd2942c0ed
coryswainston <swainston.cory89@gmail.com>
2018-11-16 23:26:02 -0700

    Add basic upload

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-var express = require('express')
-var app = express()
-var firebase = require("firebase");
+const express = require('express');
+const app = express();
+const multer = require('multer');
+const memoryStorage = multer.memoryStorage;
+const {Storage} = require('@google-cloud/storage');
+const googleCloudStorage = new Storage({
+  projectId: 556107747776,
+});
+const bucket = googleCloudStorage.bucket("memes2018");
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAvZa2qG10RV63Hy90NWy0K7d5eWycS1AA",
-    authDomain: "invertible-now-222800.firebaseapp.com",
-    databaseURL: "https://invertible-now-222800.firebaseio.com",
-    projectId: "invertible-now-222800",
-    storageBucket: "invertible-now-222800.appspot.com",
-    messagingSenderId: "556107747776"
-  };
-  firebase.initializeApp(config);
+const firebase = require('firebase');
+var config = {
+  apiKey: "AIzaSyAvZa2qG10RV63Hy90NWy0K7d5eWycS1AA",
+  authDomain: "invertible-now-222800.firebaseapp.com",
+  databaseURL: "https://invertible-now-222800.firebaseio.com",
+  projectId: "invertible-now-222800",
+  storageBucket: "invertible-now-222800.appspot.com",
+  messagingSenderId: "556107747776"
+};
+firebase.initializeApp(config);
+
+const m = multer({
+  storage: memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024 // no larger than 5mb
+  }
+});
 
 app.set('port', (process.env.PORT || 8000))
   .use(express.static(__dirname + '/public'))
@@ -22,11 +35,37 @@ app.set('port', (process.env.PORT || 8000))
   .get('/', main)
   .get('/create', create)
   .get('/account', account)
-  .post('/createaccount', createaccount)
+  .post('/upload', m.single('file'), uploadFileToCloudStorage)
   .get('*', send404)
   .listen(app.get('port'), () => console.log('Listening on ' + app.get('port')));
 
+async function uploadFileToCloudStorage(req, res, next) {
+  const blob = bucket.file(req.file.originalname);
+  const blobStream = blob.createWriteStream({
+    metadata: {
+      contentType: req.file.mimetype
+    }
+  });
+
+  blobStream.on("error", err => {
+    console.error(err);
+  });
+
+  blobStream.on("finish", () => {
+    const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+
+    blob.makePublic().then(() => {
+      res.status(200).json({url: publicUrl});
+    });
+  });
+
+  blobStream.end(req.file.buffer);
+}
+
 function main(req, res) {
+
+
+
   var images = [
     {
       name: "Wow this a cool meme",
@@ -48,19 +87,6 @@ function create(req, res) {
 
 function account(req, res){
   res.render('pages/account.ejs')
-}
-
-function createaccount(req, res){
-  var email = req.body.email;
-  var password= req.body.password;
-  console.log(email, password)
-
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    console.log(error.code);
-    console.log(error.message);
- });
-
- res.redirect("/");
 }
 
 function send404(req, res) {
